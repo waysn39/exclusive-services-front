2946603bc6b463277629ab25dcad2450cf06ff41
waysn39 <303376023@qq.com>
2022-01-23 14:55:43 +0800

    提交随机文字

---
+++ src/views/blog/bloglineword.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="mod-blog__bloglineword">
+    <el-form :inline="true" :model="dataForm" @keyup.enter="getDataList()">
+      <el-form-item>
+        <el-input v-model="dataForm.lineWord" placeholder="一句话" clearable></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button @click="getDataList()">{{ $t("query") }}</el-button>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="info" @click="exportHandle()">{{ $t("export") }}</el-button>
+      </el-form-item>
+      <el-form-item>
+        <el-button v-if="hasPermission('blog:bloglineword:save')" type="primary" @click="addOrUpdateHandle()">{{ $t("add") }}</el-button>
+      </el-form-item>
+      <el-form-item>
+        <el-button v-if="hasPermission('blog:bloglineword:delete')" type="danger" @click="deleteHandle()">{{ $t("deleteBatch") }}</el-button>
+      </el-form-item>
+    </el-form>
+    <el-table v-loading="dataListLoading" :data="dataList" border @selection-change="dataListSelectionChangeHandle" style="width: 100%">
+      <el-table-column type="selection" header-align="center" align="center" width="50"></el-table-column>
+      <el-table-column prop="lineWord" label="一句话" header-align="center" align="center"></el-table-column>
+      <el-table-column :label="$t('handle')" fixed="right" header-align="center" align="center" width="150">
+        <template v-slot="scope">
+          <el-button v-if="hasPermission('blog:bloglineword:update')" type="text" size="small" @click="addOrUpdateHandle(scope.row.id)">{{ $t("update") }}</el-button>
+          <el-button v-if="hasPermission('blog:bloglineword:delete')" type="text" size="small" @click="deleteHandle(scope.row.id)">{{ $t("delete") }}</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    <el-pagination :current-page="page" :page-sizes="[10, 20, 50, 100]" :page-size="limit" :total="total" layout="total, sizes, prev, pager, next, jumper" @size-change="pageSizeChangeHandle" @current-change="pageCurrentChangeHandle"> </el-pagination>
+    <!-- 弹窗, 新增 / 修改 -->
+    <add-or-update v-if="addOrUpdateVisible" ref="addOrUpdate" @refreshDataList="getDataList"></add-or-update>
+  </div>
+</template>
+
+<script lang="ts">
+import useView from "@/hooks/useView";
+import { defineComponent, reactive, toRefs } from "vue";
+import AddOrUpdate from "./bloglineword-add-or-update.vue";
+export default defineComponent({
+  components: {
+    AddOrUpdate
+  },
+  setup() {
+    const state = reactive({
+      getDataListURL: "/blog/bloglineword/page",
+      getDataListIsPage: true,
+      exportURL: "/blog/bloglineword/export",
+      deleteURL: "/blog/bloglineword",
+      deleteIsBatch: true,
+      dataForm: {
+        lineWord: ""
+      }
+    });
+    return { ...useView(state), ...toRefs(state) };
+  },
+  watch: {}
+});
+</script>

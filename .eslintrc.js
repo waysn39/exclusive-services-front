ac5d6473bedc1140f1d5deceed86ae6d3ea13ed9
jinyiming <jinyiming@bianfeng.com>
2022-06-01 10:59:42 +0800

    test

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,7 @@ module.exports = {
     "plugin:vue/vue3-essential",
     "eslint:recommended",
     "@vue/typescript/recommended",
+    "plugin:prettier/recommended",
     "@vue/prettier",
     "@vue/prettier/@typescript-eslint"
   ],
